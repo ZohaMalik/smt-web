4d51f8444fa94f5b799b690ccf447385d7213329
ZohaMalik <zohasmalik@gmail.com>
2024-11-24 03:57:37 +0500

    Beneficiaries Screen Added

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 // views
 import Home from '@/views/home.vue';
+import Beneficiaries from '@/views/beneficiaries.vue';
 
 const routes = [
-    { path: '/', name: Home, component: Home }
+    { path: '/', name: Home, component: Home },
+    { path: '/beneficiaries', name: Beneficiaries, component: Beneficiaries }
 ];
 
 const router = createRouter({
---
+++ src/views/beneficiaries.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import BeneficiariesIcon from '@/assets/images/beneficiaries/beneficiaries.svg';
+
+</script>
+
+<template>
+    <div class="screenContainerDiv fade-in">
+        <h1 class="font-extrabold text-4xl mb-5">Beneficiaries</h1>
+        <p class="mb-5">The beneficiaries of the trust shall be public at large.</p>
+        <img id="beneficiariesImage" :src="BeneficiariesIcon" alt="Beneficiaries Image" />
+    </div>
+</template>
+
+<style scoped>
+
+#beneficiariesImage {
+    height: 700px;
+    margin-left: auto;
+}
+
+@media screen and (max-width: 800px) { #beneficiariesImage { height: 500px; } }
+@media screen and (max-width: 500px) { #beneficiariesImage { height: 300px; } }
+
+</style>
