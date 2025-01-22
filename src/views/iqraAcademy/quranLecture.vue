1041bfbab02de624fe53ee7814569651c675d3b6
ZohaMalik <zohasmalik@gmail.com>
2025-01-22 10:10:31 +0500

    Quran Lecture Updated

--- src/views/iqraAcademy/quranLecture.vue
+++ src/views/iqraAcademy/quranLecture.vue
@@ -8,7 +8,7 @@ import OpenInNewTabImage from '@/assets/images/iqraTaleemUlQuranAcademy/QuranLec
 <template>
     <div class="screenContainerDiv fade-in text-center sm:text-left bg-green-100">
         <h1 class="font-extrabold text-4xl mb-5">Al Quran Lecture</h1>
-        <div class="font-medium mb-5">English Translation of Quran Audio Lectures: (From Surat ul-Fateha to Surat ul-Baqara Verse 230)</div>
+        <div class="font-medium mb-5">English Translation of Quran Audio Lectures: (From Surat ul-Fateha to Surat ul-Baqara Verse 250)</div>
         <div class="border rounded-lg overflow-hidden shadow-md w-3/4 mx-auto">
             <!-- PDF Preview -->
             <iframe :src="QuranLecuturePDF" class="w-full h-96" frameborder="0"></iframe>
